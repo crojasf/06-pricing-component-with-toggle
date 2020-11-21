0f0a02451590d61bffe89d6d948b943551d79885
CarlosRojasF <crojasf@gmail.com>
2020-11-21 19:21:54 +0100

    funcionalidad JavaScript a botón toggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,20 @@
 // alert("Hello world!!");
 
 checkbox = document.getElementById('switchBtn');
+prices = document.querySelectorAll('.card');
+// console.log(prices);
 
 checkbox.addEventListener( 'change', function() {
-	if(this.checked) {
-			// Checkbox is checked..
-			console.log('Checkbox is checked');
+	if(!this.checked) {
+			// Checkbox is not checked = Monthly
+			prices.forEach(logArrayElements);
 	} else {
-			// Checkbox is not checked..
-			console.log('Checkbox is not checked');
+			// Checkbox is checked = Annually
+			prices.forEach(logArrayElements);
 	}
 });
 
+function logArrayElements(element, index, array) {
+	element.children[1].classList.toggle('hidden');
+	element.children[2].classList.toggle('hidden');
+}
